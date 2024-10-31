379f180b646dfd1be37ebc54816493e604541fa3
Soheil-Scaot <soheillop1384@gmail.com>
2024-10-31 06:14:27 -0700

    emtehan

---
+++ root/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Testimoials , Services , Services_two , Prices , Pros , FQA
+
+admin.site.register(Testimoials)
+admin.site.register(Pros)
+admin.site.register(Services)
+admin.site.register(Prices)
+admin.site.register(FQA)
+admin.site.register(Services_two)
